7de708f6011b478154eae76efa6e4adcef9d6005
yasheks <143996832+yasheks@users.noreply.github.com>
2024-05-06 10:41:59 +0300

    Delete chislo+x_chislo*y.py

--- chislo+x_chislo*y.py
+++
@@ -1,8 +0,0 @@
-def calk(begin, end):
-    if begin > end:
-        return 0
-    if begin == end:
-        return 1
-    return calk(begin + 2, end) + calk(begin *5, end)
-#задача где к число прибавляется 2 и где число умножается на 5
-print(calk(2,50))
