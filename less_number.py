b8074239fc0e6d488c6c27235883034716611287
yasheks <143996832+yasheks@users.noreply.github.com>
2024-05-06 10:44:23 +0300

    Create less_number.py

---
+++ less_number.py
@@ -0,0 +1,9 @@
+def calk(begin, end):
+    if begin == 15:
+        return 0
+    #это используется когда число не нужно считать
+    if begin > end:
+        return 0
+    if begin == end:
+        return 1
+    return calk(begin + 1, end) + calk(begin +3, end)
